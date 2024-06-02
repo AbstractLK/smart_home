93b7e452acb7483e86f4d73339dabd23bc4c64b3
Kaveesha <kalshandhanasekara@gmail.com>
2024-06-03 02:49:37 +0530

    add OTA function

--- ESP32_Blynk_2.0/ESP32_Blynk_2.0.ino
+++ ESP32_Blynk_2.0/ESP32_Blynk_2.0.ino
@@ -43,7 +43,11 @@ char pass[] = "1234five";
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
+#include <ESPmDNS.h>
+//#include <NetworkUdp.h>
+#include <ArduinoOTA.h>
 #include <Preferences.h>
+
 // #include <IRremote.h>
 // #include <DHT.h>  
 
@@ -507,6 +511,9 @@ void getRelayState()
 void setup()
 {
   Serial.begin(9600);
+  Serial.println("Booting...");
+  //WiFi.mode(WIFI_STA);
+
   //Open namespace in read-write mode
   pref.begin("Relay_State", false);
   
@@ -561,6 +568,45 @@ void setup()
   Blynk.config(auth);
   delay(1000);
 
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
   getRelayState(); //fetch data from NVS Flash Memory
 //  delay(1000);
 }
@@ -569,6 +615,8 @@ void loop()
 {  
   Blynk.run();
   timer.run(); // Initiates SimpleTimer
+
+  ArduinoOTA.handle();
 
   manual_control();
   
